bc74b1a012f5aa3384e5875d97127624d50c9c83
dc <18829711180@163.com>
2019-04-29 16:11:55 +0800

    登录装饰器

--- kesheer/kesheer/utils/commons.py
+++ kesheer/kesheer/utils/commons.py
@@ -1,6 +1,10 @@
 # -- coding: utf-8 --
 
+from flask import session, jsonify, g
 from werkzeug.routing import BaseConverter
+import functools
+
+from kesheer.utils.response_code import RET
 
 
 # 定义正则转换器
@@ -13,5 +17,21 @@ class ReConverter(BaseConverter):
         self.regex = regex
 
 
+# 定义验证登陆状态的装饰器
+def login_required(view_func):
 
+    @functools.wraps(view_func)
+    def wrapper(*args, **kwargs):
+        # 判断用户登陆状态
+        user_id = session.get("user_id")
+        print("用户id", user_id)
+        # 如果用户是登陆的，直接执行试图函数
+        if user_id is not None:
+            # 全局g对象，用来传递参数
+            g.user_id = user_id
+            return view_func(*args, **kwargs)
+        else:
+            # 如果未登录，返回未登陆信息（如果是前后端不分离，直接跳转登陆页面）
+            return jsonify(errno=RET.SESSIONERR, errmsg="用户未登陆")
+    return wrapper
 
